835300bf4da95622e408c9b712b93b0e5d331c08
Ralf Naujokat <me@ralfn.de>
2025-04-21 11:07:03 +0200

    Vehicle service added

---
+++ rest-server/src/main/java/de/ralfn/pub/small_world/rest_server/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * openAPI / Swagger Docs
+ *
+ * @see <a href="https://github.com/swagger-api/swagger-core/">Docs on GitHub</a>
+ * @see <a href="https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Getting-started">Getting started</a>
+ * @see <a href="https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Annotations">Annotations</a>
+ */
+package de.ralfn.pub.small_world.rest_server.api;
